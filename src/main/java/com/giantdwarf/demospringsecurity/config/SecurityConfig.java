209151f89ef8ded18eb4c0c0d139cdf23f6fade9
seungin yang <rhfpdk92@naver.com>
2020-07-25 18:19:52 +0900

    Use csrf token

--- src/main/java/com/giantdwarf/demospringsecurity/config/SecurityConfig.java
+++ src/main/java/com/giantdwarf/demospringsecurity/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info","/account/**").permitAll()
+                .mvcMatchers("/", "/info","/account/**","signup").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
         http.formLogin();
